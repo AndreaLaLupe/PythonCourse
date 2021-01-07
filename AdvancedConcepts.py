3bedf539443e2aaa2fe7cb54987f0bf0b98a1e70
Andrea Alvarez <aalvarez2014ig@gmail.com>
2021-01-07 11:16:30 -0600

    Third part of the course

---
+++ AdvancedConcepts.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+from MyModule import foo, bar, foobar
+
+def my_function(one,two,
+                three, four,
+                five, six):
+    print("Hello World")
+
+
+def second_function():
+    print("Second function")
+
+my_list=[1,2,
+         3,4,
+         5,6]
